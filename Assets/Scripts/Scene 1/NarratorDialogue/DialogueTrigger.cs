33462734544dfc4e1ac3f1bf63c548f7c642d0aa
riyiniiii <rl5g23@soton.ac.uk>
2026-02-24 16:54:30 +0000

    trigger working finlally

--- Assets/Scripts/Scene 1/NarratorDialogue/DialogueTrigger.cs
+++ Assets/Scripts/Scene 1/NarratorDialogue/DialogueTrigger.cs
@@ -4,44 +4,46 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    [Header("Visual Cue")] 
+    [Header("Visual Cue")]
     [SerializeField] private GameObject visualCue;
+
+    [Header("Emote Animator")]
+    [SerializeField] private Animator emoteAnimator;
+
     [Header("Ink JSON")]
     [SerializeField] private TextAsset inkJSON;
-    
+
     private bool playerInRange;
-    private void Awake()
+
+    private void Awake() 
     {
         playerInRange = false;
         visualCue.SetActive(false);
     }
 
-    private void Update()
+    private void Update() 
     {
-        if (playerInRange)
+          if (playerInRange)
         {
             visualCue.SetActive(true);
-            if(InputManager.GetInstance().GetInteractPressed())
-            {
-                Debug.Log(inkJSON.text);
-            }
         }
-        else
+        else 
         {
             visualCue.SetActive(false);
         }
     }
-    private void OnTriggerEnter2D(Collider2D other)
+
+    private void OnTriggerEnter2D(Collider2D collider) 
     {
-        if (GetComponent<Collider>().gameObject.tag == "Player")
+        if (collider.gameObject.tag == "Player")
         {
             playerInRange = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D collider) 
     {
-        if (GetComponent<Collider>().gameObject.tag == "Player")
+        if (collider.gameObject.tag == "Player")
         {
             playerInRange = false;
         }
