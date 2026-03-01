a1705e5d7c424972cfee3a0f05037466a9aefa10
riyiniiii <rl5g23@soton.ac.uk>
2026-03-01 18:03:41 +0000

    Parallax Background

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -1,22 +1,22 @@
-using System.Numerics;
+using System;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
+public class parallax : MonoBehaviour
+{
+    Material mat;
+    public float distance;
 
-            Material mat;
-            public float speed=0.2f;
+    [Range(0f, 0.5f)] public float speed = 0.2f;
 
-            [Range((0f,0.5f))] 
-            public float speed= 0.2f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         mat = GetComponent<Renderer>().material;
     }
 
     void Update()
-    { 
-        distance += Time,deltaTime* speed;
-        mat.SetTextureOffest("_Main text",Vector.right * distance)
+    {
+        distance += Time.deltaTime*speed;
+        mat.SetTextureOffset("_MainTex", Vector2.right * distance);
     }
 }
+
