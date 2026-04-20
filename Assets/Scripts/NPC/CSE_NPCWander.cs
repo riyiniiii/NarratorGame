71b4d86d32069febda1569eab9f4c8bbad1ef29a
https://github.com/riyiniiii/NarratorGame <rl5g23@soton.ac.uk>
2026-04-20 23:55:26 +0100

    Added 'Sheep' Npc
    -animation
    -running around

--- Assets/Scripts/NPC/CSE_NPCWander.cs
+++ Assets/Scripts/NPC/CSE_NPCWander.cs
@@ -7,21 +7,53 @@ public class CSE_NPCWander : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed;
     [SerializeField] private float leftPatrolX, rightPatrolX;
+    [SerializeField] private Animator anim;
+    
+    [SerializeField] private float minPauseTime, maxPauseTime;
+    [SerializeField] private float minWalkTime, maxWalkTime;
     
     [SerializeField] private int facingDirection = -1;
+
+    private bool isFlipping;
+    private float randomTime, timer;
+    private bool isWalking = true;
+
+    private void Start()
+    {
+        randomTime = Random.Range(minWalkTime, maxWalkTime);
+        anim.SetBool("isWalking", isWalking ? true : false);
+    }
  
     void Update()
     {
-        if (transform.position.x > rightPatrolX || transform.position.x < leftPatrolX)
-            Flip();
+        timer += Time.deltaTime;
 
+        if (timer >= randomTime)
+            StateChange();
+        
+        if (!isFlipping && (transform.position.x > rightPatrolX || transform.position.x < leftPatrolX))
+            StartCoroutine(Flip());
+        
+        if (isWalking)
         rb.linearVelocity = Vector2.right * facingDirection * speed;
-
     }
-
-    void Flip()
+    
+    IEnumerator Flip()
     {
+        isFlipping = true;
         transform.Rotate(0, 180, 0);
         facingDirection *= -1;
+        yield return new WaitForSeconds(0.5f);
+        isFlipping = false;
     }
+
+    void StateChange()
+    {
+        isWalking = !isWalking;
+        anim.SetBool("isWalking", isWalking ? true : false);
+        randomTime = isWalking ? Random.Range(minWalkTime , maxWalkTime) : Random.Range(minPauseTime, maxPauseTime);
+        timer = 0;
+    }
+    
+    
 }
