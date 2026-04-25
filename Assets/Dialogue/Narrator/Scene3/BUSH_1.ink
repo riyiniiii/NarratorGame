e847f9c170248e86dd1c1474e4e52c96ec5d1a33
https://github.com/riyiniiii/NarratorGame <rl5g23@soton.ac.uk>
2026-04-25 14:57:22 +0100

    SC3.1
    Adding more dialogue

---
+++ Assets/Dialogue/Narrator/Scene3/BUSH_1.ink
@@ -0,0 +1,8 @@
+#speaker:Narrator #portrait:Narrator 
+<i>“Hmm. I don't recall seeing a <b>wicked wolf</b> or a <b>lamb</b> in this tale. Do not tell me I put you in the wrong storyline. "</i> 
+
+ +[?] 
+
+<i> "For now, let's carry on... As the brave witch made her way deeper into the forest, she quickened her pace in order to get home and see her most cherished cat." </i> 
+
+->END
--- Assets/Scripts/CS_3/BushTrigger.cs
+++ Assets/Scripts/CS_3/BushTrigger.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
+using System.Collections;
 
-    public class BushTrigger : MonoBehaviour
+public class BushTrigger : MonoBehaviour
+{
+    private AudioManager audioManager;
+    private bool hasPlayed = false;
+
+    private void Start()
     {
-        private AudioManager audioManager;
+        audioManager = FindFirstObjectByType<AudioManager>();
+    }
 
-        private void Start()
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !hasPlayed)
         {
-            audioManager = FindFirstObjectByType<AudioManager>();
-        }
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            if (other.CompareTag("Player"))
-            {
-                audioManager.PlaySFX(audioManager.growling);
-            }
+            hasPlayed = true;
+            StartCoroutine(PlayDelayedSound());
         }
     }
+
+    private IEnumerator PlayDelayedSound()
+    {
+        yield return new WaitForSeconds(0.5f);
+        audioManager.PlaySFX(audioManager.growling);
+    }
+}
