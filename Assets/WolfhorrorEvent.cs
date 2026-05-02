29d15587b549c53807c7ba793bb4776f6e92bd5a
https://github.com/riyiniiii/NarratorGame <rl5g23@soton.ac.uk>
2026-05-02 18:55:53 +0100

    Wolf bugs fixed
    - Can hide in bush
    - wolf moves aside
    - wolf stop chasing for a bit
    - able to move after dialogue ends

--- Assets/WolfhorrorEvent.cs
+++ Assets/WolfhorrorEvent.cs
@@ -4,77 +4,107 @@ using System.Collections;
 public class WolfHorrorEvent : MonoBehaviour
 {
     public CharacterController2D player;
-    public Transform wolf;
+    public WolfStalking wolf;
+    public Transform bushHidePoint;
     public TextAsset inkJSON;
 
-    public float wolfSpeed = 3f;
+    public float approachSpeed = 2f;
+    public float stopDistance = 0.1f;
+    public float spawnDistanceRight = 4f;
+    public float bushOffsetLeft = 1.5f;
 
-    public void StartHideCountdown()
+    private bool eventActive;
+
+    public void TriggerEvent()
     {
-        StartCoroutine(HideCountdown());
+        if (eventActive) return;
+
+        StartCoroutine(HorrorSequence());
     }
 
-    IEnumerator HideCountdown()
+    private IEnumerator HorrorSequence()
     {
-        yield return new WaitForSeconds(5f);
+        eventActive = true;
 
-        if (player.isHidden)
+        // freeze player
+        player.canMove = false;
+
+        // reset wolf visuals/state
+        wolf.ResetForHorrorEvent();
+
+        yield return new WaitForSeconds(0.5f);
+
+        // spawn wolf to the right of player
+        Vector3 spawnPos = player.transform.position + Vector3.right * spawnDistanceRight;
+        spawnPos.y = wolf.transform.position.y; // lock Y
+        wolf.transform.position = spawnPos;
+
+        yield return new WaitForSeconds(0.5f);
+
+        // target position: slightly left of bush (HORIZONTAL ONLY)
+        Vector3 targetPos = new Vector3(
+            bushHidePoint.position.x - bushOffsetLeft,
+            wolf.transform.position.y,
+            wolf.transform.position.z
+        );
+
+        // move wolf in straight line (no vertical drift)
+        while (Vector2.Distance(
+            new Vector2(wolf.transform.position.x, 0),
+            new Vector2(targetPos.x, 0)
+        ) > stopDistance)
         {
-            Debug.Log("Player hid in time");
-            StartCoroutine(HorrorSequence());
-        }
-        else
-        {
-            Debug.Log("Player failed to hide!");
-
-            // Put jumpscare or chase logic here
-        }
-    }
-
-    IEnumerator HorrorSequence()
-    {
-        CameraFollow cam = Camera.main.GetComponent<CameraFollow>();
-        if (cam != null)
-            cam.enabled = false;
-
-        Camera.main.transform.position = player.transform.position + new Vector3(0, 0, -10);
-
-        yield return new WaitForSeconds(1f);
-
-        Vector3 startPos = player.transform.position + new Vector3(8f, 0, 0);
-        Vector3 endPos = player.transform.position + new Vector3(-8f, 0, 0);
-
-        wolf.position = startPos;
-
-        while (Vector2.Distance(wolf.position, endPos) > 0.1f)
-        {
-            wolf.position = Vector2.MoveTowards(
-                wolf.position,
-                endPos,
-                wolfSpeed * Time.deltaTime
+            Vector3 pos = Vector2.MoveTowards(
+                wolf.transform.position,
+                targetPos,
+                approachSpeed * Time.deltaTime
             );
+
+            // force Y to stay fixed
+            pos.y = wolf.transform.position.y;
+            wolf.transform.position = pos;
 
             yield return null;
         }
 
-        if (inkJSON != null)
-        {
-            DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+        // ensure wolf is visible
+        SpriteRenderer sr = wolf.GetComponent<SpriteRenderer>();
+        sr.enabled = true;
 
-            yield return new WaitUntil(() => !DialogueManager.GetInstance().dialogueIsPlaying);
+        Color c = sr.color;
+        c.a = 1f;
+        sr.color = c;
+
+        // start dialogue while wolf is present
+        DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+
+        // wait for dialogue to finish
+        while (DialogueManager.GetInstance().dialogueIsPlaying)
+        {
+            yield return null;
         }
 
-        EndEvent();
+        // fade wolf out after dialogue
+        yield return StartCoroutine(VanishWolf(sr));
     }
 
-    public void EndEvent()
+    private IEnumerator VanishWolf(SpriteRenderer sr)
     {
+        float fadeSpeed = 2f;
+
+        while (sr.color.a > 0.05f)
+        {
+            Color c = sr.color;
+            c.a = Mathf.Lerp(c.a, 0f, Time.deltaTime * fadeSpeed);
+            sr.color = c;
+
+            yield return null;
+        }
+
+        sr.enabled = false;
+
+        eventActive = false;
+        player.canMove = true;
         player.SetHidden(false);
-
-        CameraFollow cam = Camera.main.GetComponent<CameraFollow>();
-        if (cam != null)
-            cam.enabled = true;
-
-        Debug.Log("Event ended. Player can move again.");
     }
 }
